771b0bd190e55c512f1c0f805995bce5daed9089
Adarsh <123921544+adarshknight@users.noreply.github.com>
2024-02-21 09:41:18 +0530

    Add files via upload
    727722EUIT006-CC1

---
+++ springproject/project/src/main/java/com/example/project/model/Book.java
@@ -0,0 +1,42 @@
+package com.example.project.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="Book")
+public class Book{
+    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+    String bookname;
+    String author;
+    int price;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getBookname() {
+        return bookname;
+    }
+    public void setBookname(String bookname) {
+        this.bookname = bookname;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    public int getPrice() {
+        return price;
+    }
+    public void setPrice(int price) {
+        this.price = price;
+    }
+    
+}
